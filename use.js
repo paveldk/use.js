c0ea2017c53b0213cda945d1533e03631f3eaef5
tbranyen <tim@tabdeveloper.com>
2012-02-22 16:50:53 -0500

    pushing up 0.3.0 changes

--- use.js
+++ use.js
@@ -1,19 +1,39 @@
-/* RequireJS Use Plugin v0.2.0
+/* RequireJS Use Plugin v0.3.0
  * Copyright 2012, Tim Branyen (@tbranyen)
  * use.js may be freely distributed under the MIT license.
  */
-(function() {
+(function(window) {
 
 // Cache used to map configuration options between load and write.
 var buildMap = {};
+// Short-hand the toString method for later comparison
+var toString = Object.prototype.toString;
+
+function keysAndValues(obj) {
+  var key;
+  var keys = [];
+  var values = [];
+
+  for (key in obj) {
+    if (!obj.hasOwnProperty(key)) { continue; }
+
+    // Add keys and values to end of each respective array
+    keys.push(key);
+    values.push(obj[key]);
+  }
+
+  return [keys, values];
+}
 
 define({
-  version: "0.2.0",
+  version: "0.3.0",
 
   // Invoked by the AMD builder, passed the path to resolve, the require
   // function, done callback, and the configuration options.
   load: function(name, req, load, config) {
+    var i, buildModule;
     var module = config.use && config.use[name];
+    var random = "use_" + Math.floor(Math.random()*10000)+10000;
 
     // No module to load so return early.
     if (!module) {
@@ -21,24 +41,76 @@ define({
     }
 
     // Attach to the build map for use in the write method below.
-    buildMap[name] = { deps: module.deps || [], attach: module.attach };
+    buildModule = buildMap[name] = {
+      identifiers: [],
+      deps: [],
+      attach: module.attach
+    };
+
+    // Create the identifiers and dependencies lists
+    if (module.deps && module.deps.length) {
+      for (i=0; i<module.deps.length; i++) {
+        if (typeof module.deps[i] === "string") {
+          buildModule.identifiers.push(random);
+          buildModule.deps.push(module.deps[i]);
+
+        } else {
+          keyVals = keysAndValues(module.deps[i]);
+
+          // Assign the identifiers and dependencies to the keys and values
+          // respectively and make sure all keys are valid either by choice or
+          // random.
+          buildModule.identifiers.push(keyVals[0][0]);
+          buildModule.deps.push(keyVals[1][0]);
+        }
+      }
+    }
 
     // Read the current module configuration for any dependencies that are
     // required to run this particular non-AMD module.
-    req(module.deps || [], function() {
+    req(buildModule.deps, function() {
+      var key, val;
+      var globals = {};
+
+      // Here we need to inject into the global scope 
+      for (i=0; i<buildModule.identifiers.length; i++) {
+        // Short-hand the key/vals
+        key = buildModule.identifiers[i];
+        val = arguments[i];
+
+        // Save original copy
+        globals[key] = window[key];
+
+        // Map key to val on the window object
+        window[key] = val;
+      }
+      
       // Require this module
       req([name], function() {
+        var retVal;
         // Attach property
         var attach = module.attach;
 
         // If doing a build don't care about loading
-        if (config.isBuild) { 
+        if (config.isBuild) {
           return load();
         }
 
         // Return the correct attached object
         if (typeof attach === "function") {
-          return load(attach.apply(window, arguments));
+          retVal = load(attach.apply(window, arguments));
+        }
+
+        // Reset the global scope
+        for (i=0; i<buildModule.identifiers.length; i++) {
+          // Short-hand the key/vals
+          key = buildModule.identifiers[i];
+
+          window[key] = globals[key];
+        }
+
+        if (retVal) {
+          return retVal;
         }
 
         // Use window for now (maybe this?)
@@ -81,4 +153,4 @@ define({
   }
 });
 
-})();
+})(this);
